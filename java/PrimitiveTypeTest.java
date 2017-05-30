4cd877d4c39d0aa13fdcb2f74a53354dee8ffb03
alex <liufangyuan111@126.com>
2017-05-30 20:49:49 +0800

    java

---
+++ java/PrimitiveTypeTest.java
@@ -0,0 +1,8 @@
+public class PrimitiveTypeTest {
+    public static void main(String[] args) {
+        System.out.println("byte's size is " + Byte.SIZE);
+        System.out.println();
+
+        
+    }
+}
