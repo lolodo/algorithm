2adcf03d78aacbc9d1f90f8b60853c0b526d2ea8
alex <liufangyuan111@126.com>
2017-06-22 21:27:34 +0800

    sort

---
+++ java/SortFunc/BubbleSort.java
@@ -0,0 +1,35 @@
+public class BubbleSort {
+    public static void bubblesort (int[] number) {
+        int max;
+        int flag;
+
+        if(number.length <= 1) {
+            System.out.println("number.length is " + number.length);
+            return;
+        }
+
+        for(int i = number.length - 1; i > 0 ; i--) {
+            flag = 0;
+            for (int j = 0; j < i; j++) {
+                if (number[j] > number[j + 1]) {
+                    max = number[j];
+                    number[j] = number[j + 1];
+                    number[j + 1] = max;
+                    flag = 1;
+                }
+            }
+
+            if(flag == 0) {
+                break;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int[] array = {23,65,2,0, 3};
+        bubblesort(array);
+
+        for (int num:array) {
+            System.out.print(num + " ");
+        }
+    }
+}
