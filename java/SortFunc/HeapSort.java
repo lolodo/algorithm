f3fc452fee701acdc2207a017e38e53ecae3b507
alex <liufangyuan111@126.com>
2017-06-23 21:02:14 +0800

    heap sort

--- java/SortFunc/HeapSort.java
+++ java/SortFunc/HeapSort.java
@@ -1,7 +1,31 @@
+import java.util.Arrays;
 public class HeapSort {
+    public static void buildHeap(int[] array, int last) {
+        int i;
+        int max;
 
+        for(i = (last - 1) / 2; i >= 0; i--) {
+            int k = 2 * i + 1;//left
+            int biggerest = k;
+
+            if (k < last) {
+                if(array[k] < array[k + 1]) {
+                    biggerest++;
+                }
+            }
+
+            if(array[biggerest] > array[i]) {
+                max = array[biggerest];
+                array[biggerest] = array[i];
+                array[i] = max;
+            }
+        }
+    }
     public static void heapSort(int[] array) {
-        
+        int i;
+
+        for (i = 0; i < array.length; i++)
+        buildHeap(array, array.length - 1)
     }
     public static void main(String[] args) {
         int[] a = ShellSort.array;
@@ -12,6 +36,7 @@ public class HeapSort {
 
         heapSort(a);
 
-        ShellSort.printArray(a);
+        System.out.println(Arrays.toString(a));
+        // ShellSort.printArray(a);
     }
 }
