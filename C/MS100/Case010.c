a57e7eacc81d13dd1ff4d907f73b4ca2c67bd06b
alex <liufangyuan111@126.com>
2017-07-02 21:57:23 +0800

    good

--- C/MS100/Case010.c
+++ C/MS100/Case010.c
@@ -7,4 +7,76 @@ Answer:
 Already done this. Skipped.
 */
 
-//skipped
+#include <stdio.h>
+#include <string.h>
+
+void reverse(char *start, char *end) {
+    if(start == NULL || end == NULL) {
+        return;
+    }
+
+    while(start < end) {
+        char temp = *start;
+        *start = *end;
+        *end = temp;
+        start++, end--;
+    }
+}
+
+void reverse_sentence(char *string)
+{
+    int len;
+    int i, j;
+    int start, end;
+    char *start_c, *end_c;
+    char c;
+
+    if (string == NULL) {
+        printf("string is null!\n");
+        return;
+    }
+
+    len = strlen(string);
+    if (len <= 1) {
+        printf("this string only has one character!\n");
+        return;
+    }
+
+    start_c = string;
+    end_c = string + len - 1;
+    reverse(start_c, end_c);
+
+    i = 0;
+    while(1) {
+        while(string[i] == ' ') {
+            i++;
+        }
+
+        start = i;
+        
+        while(string[i] != ' ' && string[i] != '\0') {
+            i++;
+        }
+
+        end = i;
+
+        start_c = string + start;
+        end_c = string + end - 1;
+        reverse(start_c, end_c);
+
+        if (string[i] == '\0'){
+            break;
+        }
+    }
+}
+
+int main(int argc, char *args[])
+{
+    // char s1[] = "I am a studdent.";
+    // char s1[]= "   ";
+    
+    reverse_sentence(args[1]);
+    printf("1:%s\n", args[1]);
+
+    return 0;
+}
