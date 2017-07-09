e721d751593384b61c170ad94935b4a5e9ab5b46
alex <liufangyuan111@126.com>
2017-07-09 19:30:37 +0800

    new

---
+++ C/MS100/Case023.c
@@ -0,0 +1,15 @@
+/*
+用最简单，最快速的方法计算出下面这个圆形是否和正方形相交。"
+3D 坐标系原点(0.0,0.0,0.0)
+圆形:
+半径r = 3.0
+圆心o = (*.*, 0.0, *.*)
+正方形:
+4 个角坐标;
+1:(*.*, 0.0, *.*)
+2:(*.*, 0.0, *.*)
+3:(*.*, 0.0, *.*)
+4:(*.*, 0.0, *.*)
+ANSWER
+Crap... I totally cannot understand this problem... Does the *.* represent any possible number?
+*/
