4b78e523eec275e64115592f3eb8bed328a13ae1
alex <liufangyuan111@126.com>
2017-08-09 20:15:28 +0800

    new

--- Deques_and_Randomized_Queues/src/RandomizedQueue.java
+++ Deques_and_Randomized_Queues/src/RandomizedQueue.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+import java.util.Iterator;
+
 public class RandomizedQueue<Item> implements Iterable<Item> {
     private  Item[] elems = (Item[]) new Object[0];
     private  int numElems = 0;
@@ -17,12 +21,108 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
         int count = 0;
         for (Item i:this) {
-
+            count++;
         }
+
+        return count;
     }                        // return the number of items on the queue
-    public void enqueue(Item item)           // add the item
-    public Item dequeue()                    // remove and return a random item
-    public Item sample()                     // return (but do not remove) a random item
-    public Iterator<Item> iterator()         // return an independent iterator over items in random order
-    public static void main(String[] args)   // unit testing (optional)
+
+    public void enqueue(Item item) {
+        if (item == null) {
+            throw new java.lang.NullPointerException();
+        }
+
+        final int size = elems.length;
+        if (size == 0) {
+            resize(1);
+        } else if (size <= numElems) {
+            resize(2 * size);
+        }
+
+        elems[numElems++] = item;
+    }
+
+    private void resize(int max) {
+        final Item[] temp = (Item[]) new Object[max];
+        final int arrLength = elems.length;
+        int numElemToCopy;
+
+        if (arrLength > max) {
+            numElemToCopy = max;
+        } else {
+            numElemToCopy = arrLength;
+        }
+
+        System.arraycopy(elems, 0, temp, 0, numElemToCopy);
+        elems = temp;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+
+        int rand = StdRandom.uniform(numElems);
+        final Item obj = elems[rand];
+        elems[rand] = elems[--numElems];
+
+        final int arrLength = elems.length;
+        if (numElems > 0 && numElems == arrLength / 4) {
+            resize(arrLength / 2);
+        }
+
+        return obj;
+    }
+
+    public Item sample() {
+        if (isEmpty()) {
+            throw new java.util.NoSuchElementException();
+        }
+
+        Item obj = null;
+        while (obj == null) {
+            int rand = StdRandom.uniform(numElems);
+            obj = elems[rand];
+        }
+
+        return obj;
+    }
+
+    public Iterator<Item> iterator() {
+        return new RandomizedQueueIterator<Item>(elems);
+    }
+
+    private static class RandomizedQueueIterator<Item> implements Iterator<Item> {
+        private RandomizedQueue<Item> newColl = new RandomizedQueue<Item>();
+
+        public RandomizedQueueIterator(Object[] items) {
+            for (Object o : items) {
+                if (o == null) {
+                    break;
+                }
+
+                newColl.enqueue((Item) o);
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !newColl.isEmpty();
+        }
+
+        @Override
+        public Item next() {
+            if (newColl.isEmpty()) {
+                throw new java.util.NoSuchElementException();
+            }
+
+            return newColl.dequeue();
+        }
+
+        @Override
+        public  void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+//    public static void main(String[] args)   // unit testing (optional)
 }
