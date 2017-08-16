c531b0c54877d5b19aac24a8a5e377dafa1df9c3
alex <liufangyuan111@126.com>
2017-08-17 00:19:30 +0800

    upload

--- Deques_and_Randomized_Queues/src/Permutation.java
+++ Deques_and_Randomized_Queues/src/Permutation.java
@@ -1,21 +1,15 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
-import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
-
-        RandomizedQueue<String> rq = new RandomizedQueue<String>();
-        String[] strs = StdIn.readAllStrings();
-        StdRandom.shuffle(strs);
-
-        for (int i = 0; i < k; i++) {
-            rq.enqueue(strs[i]);
+        RandomizedQueue<String> deque = new RandomizedQueue<>();
+        while(!StdIn.isEmpty()) {
+            deque.enqueue(StdIn.readString());
         }
-
-        for (int i = 0; i < k; i++) {
-            StdOut.println(rq.dequeue());
+        for(int i = 0; i < k; i++) {
+            StdOut.println(deque.dequeue());
         }
     }
 }
--- Deques_and_Randomized_Queues/src/RandomizedQueue.java
+++ Deques_and_Randomized_Queues/src/RandomizedQueue.java
@@ -1,130 +1,87 @@
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
 import java.util.Iterator;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
-    @SuppressWarnings(value = {"unchecked"})
-    private  Item[] elems = (Item[]) new Object[0];
-    private  int numElems = 0;
+    private Item[] a;
+    private int N;
 
-    public RandomizedQueue(){
-
-    }                 // construct an empty randomized queue
-
-    public boolean isEmpty() {
-        return numElems == 0;
-    }             // is the queue empty?
-
-    public int size() {
-        if (isEmpty()) {
-            return 0;
-        }
-
-        int count = 0;
-        for (Item i:this) {
-            count++;
-        }
-
-        return count;
-    }                        // return the number of items on the queue
-
-    public void enqueue(Item item) {
-        if (item == null) {
-            throw new java.lang.NullPointerException();
-        }
-
-        final int size = elems.length;
-        if (size == 0) {
-            resize(1);
-        } else if (size <= numElems) {
-            resize(2 * size);
-        }
-
-        elems[numElems++] = item;
+    public RandomizedQueue() {               // construct an empty randomized queue
+        a = (Item[]) new Object[2];
+        N = 0;
     }
 
+    public boolean isEmpty() {                // is the queue empty?
+        return (N == 0);
+    }
+
+    public int size() {                       // return the number of items on the queue
+        return N;
+    }
     private void resize(int max) {
-        @SuppressWarnings(value = {"unchecked"})
-        final Item[] temp = (Item[]) new Object[max];
-        final int arrLength = elems.length;
-        int numElemToCopy;
-
-        if (arrLength > max) {
-            numElemToCopy = max;
-        } else {
-            numElemToCopy = arrLength;
+        Item[] temp = (Item[]) new Object[max];
+        for(int i = 0; i < N; i++) {
+            temp[i] = a[i];
         }
-
-        System.arraycopy(elems, 0, temp, 0, numElemToCopy);
-        elems = temp;
+        a = temp;
+    }
+    public void enqueue(Item item) {          // add the item
+        if(item == null) throw new java.lang.NullPointerException();
+        if(N == a.length) resize(a.length * 2);
+        a[N++] = item;
+    }
+    public Item dequeue() {                   // delete and return a random item
+        if(N == 0) throw new java.util.NoSuchElementException();
+        int index = (int)(StdRandom.uniform(N));
+        Item temp = a[index];
+        if(index != N-1) a[index] = a[N-1];
+        a[N-1] = null;
+        N--;
+        if(N >=0 && N == a.length/4) resize(a.length/2);
+        return temp;
     }
 
-    public Item dequeue() {
-        if (isEmpty()) {
-            throw new java.util.NoSuchElementException();
-        }
-
-        int rand = StdRandom.uniform(numElems);
-        final Item obj = elems[rand];
-        elems[rand] = elems[--numElems];
-
-        final int arrLength = elems.length;
-        if (numElems > 0 && numElems == arrLength / 4) {
-            resize(arrLength / 2);
-        }
-
-        return obj;
+    public Item sample() {                    // return (but do not delete) a random item
+        if(N == 0) throw new java.util.NoSuchElementException();
+        int index = (int)(StdRandom.random() * N);
+        return a[index];
     }
-
-    public Item sample() {
-        if (isEmpty()) {
-            throw new java.util.NoSuchElementException();
-        }
-
-        Item obj = null;
-        while (obj == null) {
-            int rand = StdRandom.uniform(numElems);
-            obj = elems[rand];
-        }
-
-        return obj;
+    public Iterator<Item> iterator() {        // return an independent iterator over items in random order
+        return new ReverseArrayIterator();
     }
-
-    public Iterator<Item> iterator() {
-        return new RandomizedQueueIterator<Item>(elems);
-    }
-
-    private static class RandomizedQueueIterator<Item> implements Iterator<Item> {
-        private RandomizedQueue<Item> newColl = new RandomizedQueue<Item>();
-
-        public RandomizedQueueIterator(Object[] items) {
-            for (Object o : items) {
-                if (o == null) {
-                    break;
-                }
-//                @SuppressWarnings(value = {"unchecked"})
-                newColl.enqueue((Item) o);
-            }
+    private class ReverseArrayIterator implements Iterator<Item> {
+        private int index = 0;
+        private Item[] r;
+        public ReverseArrayIterator() {
+            r = (Item[]) new Object[N];
+            for(int i=0; i<N; i++)
+                r[i] = a[i];
+            StdRandom.shuffle(r);
         }
-
-        @Override
         public boolean hasNext() {
-            return !newColl.isEmpty();
+            return index < N;
         }
-
-        @Override
+        public void remove() {
+            throw new java.lang.UnsupportedOperationException();
+        }
         public Item next() {
-            if (newColl.isEmpty()) {
-                throw new java.util.NoSuchElementException();
-            }
-
-            return newColl.dequeue();
-        }
-
-        @Override
-        public  void remove() {
-            throw new UnsupportedOperationException();
+            if(!hasNext()) throw new java.util.NoSuchElementException();
+            Item item = r[index++];
+            return item;
         }
     }
-//    public static void main(String[] args)   // unit testing (optional)
+    public static void main(String[] args) {   // unit testing
+        RandomizedQueue<String> deque = new RandomizedQueue<String> ();
+        deque.enqueue("aa");
+        deque.enqueue("bb");
+        deque.enqueue("cc");
+        deque.enqueue("dd");
+        deque.dequeue();
+        deque.dequeue();
+        deque.dequeue();
+        deque.dequeue();
+        //deque.dequeue();
+        StdOut.print("size:"+deque.size());
+    }
 }
