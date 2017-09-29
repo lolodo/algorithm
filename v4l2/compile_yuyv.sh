807a50f076868c64ce5720dfc19b32e2ddbb0de3
fangyuan <fangyuan.liu@nio.com>
2017-09-29 15:45:13 +0800

    v4l2

---
+++ v4l2/compile_yuyv.sh
@@ -0,0 +1,8 @@
+#########################################################################
+# File Name: compile_yuyv.sh
+# Author: Alex
+# mail: fangyuan.liu@nio.com
+# Created Time: Tue 26 Sep 2017 03:41:11 PM CST
+#########################################################################
+#!/bin/bash
+~/workspace/cross_compile_app/bin/aarch64-linux-gnu-g++ camera_yuyv.cpp -o mtk_yuyv
