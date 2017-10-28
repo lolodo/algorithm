61b76c6b17b71b75df1df38d65a081727df4d1f3
alex <liufangyuan111@126.com>
2017-10-29 01:32:48 +0800

    ui related

--- UIWidgetTest/app/src/main/java/com/example/liufangyuan/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/liufangyuan/uiwidgettest/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.liufangyuan.uiwidgettest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Log.d(TAG, "onClick: get it!");
+//            }
+//        });
+        editText = (EditText)findViewById(R.id.edit_text);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button:
+                String inputText = editText.getText().toString();
+                Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+//                Log.d(TAG, "onClick: button");
+                break;
+            default:
+                break;
+        }
     }
 }
