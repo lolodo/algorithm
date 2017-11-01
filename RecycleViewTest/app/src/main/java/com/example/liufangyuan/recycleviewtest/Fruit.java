f8eb222f7baf9e3855eb4f356ddd1758a96a870e
alex <liufangyuan111@126.com>
2017-11-01 23:54:30 +0800

    src

---
+++ RecycleViewTest/app/src/main/java/com/example/liufangyuan/recycleviewtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.liufangyuan.listviewtest;
+
+/**
+ * Created by liufangyuan on 2017/10/31.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageID;
+
+    public Fruit(String name, int imageID) {
+        this.name = name;
+        this.imageID = imageID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageID() {
+        return imageID;
+    }
+}
