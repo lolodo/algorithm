ab7652d9eff49a5428841ce5a3b8fa4ad1b0c899
alex <liufangyuan111@126.com>
2017-11-19 15:51:14 +0800

    android:BroadcastTest:added a program
    added a BroadcastTest application.
    
    Signed-off-by: alex <liufangyuan111@126.com>

---
+++ android_app/FragmentBestPractice/app/src/main/java/com/example/liufangyuan/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,31 @@
+package com.example.liufangyuan.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by liufangyuan on 2017/11/18.
+ */
+
+public class NewsTitleFragment extends Fragment {
+    private boolean isTwoPane;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_content_frag, container, false);
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        if (getActivity().findViewById(R.id.news_content_layout) != null) {
+
+        }
+    }
+}
