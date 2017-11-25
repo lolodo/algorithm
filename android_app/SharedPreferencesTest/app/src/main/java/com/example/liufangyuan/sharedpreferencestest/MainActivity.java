ac1236c5c0b6b36456ba29fb61b53eca488d9c9f
alex <liufangyuan111@126.com>
2017-11-25 15:55:38 +0800

    good

---
+++ android_app/SharedPreferencesTest/app/src/main/java/com/example/liufangyuan/sharedpreferencestest/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.liufangyuan.sharedpreferencestest;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.sql.BatchUpdateException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "sharedpreferencestest";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.e(TAG, "onCreate: ");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button saveData = (Button)findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "save: ");
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+                editor.putString("name", "Tom");
+                editor.putInt("age", 28);
+                editor.putBoolean("married", false);
+                editor.apply();
+            }
+        });
+
+        Button restoreData = (Button)findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: enter");
+                SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married", false);
+                Log.d(TAG, "onClick: name is " + name);
+                Log.d(TAG, "onClick: age is " + age);
+                Log.d(TAG, "onClick: married is " + married);
+            }
+        });
+    }
+}
