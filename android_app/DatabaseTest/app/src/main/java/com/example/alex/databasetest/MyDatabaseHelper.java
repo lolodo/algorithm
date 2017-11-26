1bf4616989bf1cdf03a8b32830a44bd36ea282aa
alex <liufangyuan111@126.com>
2017-11-26 16:46:27 +0800

    good

--- android_app/DatabaseTest/app/src/main/java/com/example/alex/databasetest/MyDatabaseHelper.java
+++ android_app/DatabaseTest/app/src/main/java/com/example/alex/databasetest/MyDatabaseHelper.java
@@ -18,7 +18,10 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
             + "name text)";
 
     public static final String CREATE_CATEGORY = "create table Category ("
-            + "id integer primary key autoincrement"
+            + "id integer primary key autoincrement, "
+            + "category_name text, "
+            + "category_code integer)";
+
     private Context mContext;
     public MyDatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int vertion) {
         super(context, name, factory, vertion);
@@ -28,10 +31,14 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
         sqLiteDatabase.execSQL(CREATE_BOOK);
+        sqLiteDatabase.execSQL(CREATE_CATEGORY);
         Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("drop table if exists Book");
+        sqLiteDatabase.execSQL("drop table if exists Category");
+        onCreate(sqLiteDatabase);
     }
 }
