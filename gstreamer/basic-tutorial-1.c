e3c2e3239d1df8b0a0b614d2eefceb0bd63afb47
Fangyuan Liu <fangyuan.liu@nio.com>
2017-11-30 20:25:31 +0800

    new

--- gstreamer/basic-tutorial-1.c
+++ gstreamer/basic-tutorial-1.c
@@ -4,39 +4,35 @@
     > Mail: fangyuan.liu@nio.com 
     > Created Time: Fri 29 Sep 2017 03:50:38 PM CST
  ************************************************************************/
-
 #include <gst/gst.h>
-#include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-  GstElement *pipeline;
-  GstBus *bus;
-  GstMessage *msg;
-  int ret;
+	GstElement *pipeline;
+	GstBus *bus;
+	GstMessage *msg;
 
-  /* Initialize GStreamer */
-  gst_init(&argc, &argv);
+	/* Initialize GStreamer */
+	gst_init(&argc, &argv);
 
-  /* Build the pipeline */
-  pipeline = gst_parse_launch("playbin uri=http:///www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
+	/* Build the pipeline */
+	pipeline = gst_parse_launch("playbin uri=https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm", NULL);
 
-  /* Start playing */
-  ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
-  printf("ret is %d\n", ret);
+	/* Start playing */
+	gst_element_set_state(pipeline, GST_STATE_PLAYING);
 
-  /* Wait until error or EOS */
-  bus = gst_element_get_bus(pipeline);
-  msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+	/* Waiting until error or EOS */
+	bus = gst_element_get_bus(pipeline);
+	msg = gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE, GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
 
-  /* Free resources */
-  if (msg != NULL)
-  {
-    gst_message_unref(msg);
-  }
+	/* Free resources */
+	if (msg != NULL) {
+		gst_message_unref(msg);
+	}
 
-  gst_object_unref(bus);
-  gst_element_set_state(pipeline, GST_STATE_NULL);
-  gst_object_unref(pipeline);
-  return 0;
+	gst_object_unref(msg);
+	gst_element_set_state(pipeline, GST_STATE_NULL);
+	gst_object_unref(pipeline);
+
+	return 0;
 }
