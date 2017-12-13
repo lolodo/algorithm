3b18e3d6bbae8d2dbb68755edc4029bc9ffb951f
alex <liufangyuan111@126.com>
2017-12-14 01:59:14 +0800

    good

---
+++ android_app/CameraAlbumTest/app/src/main/java/com/example/liufangyuan/cameraalbumtest/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.liufangyuan.cameraalbumtest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int TAKE_PHOTO = 1;
+    private static final String TAG = "good";
+    private ImageView picture;
+    private Uri imageUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button takePhoto = (Button)findViewById(R.id.take_photo);
+        picture = (ImageView)findViewById(R.id.picture);
+        takePhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File outputImage = new File(getExternalCacheDir(), "output_image.jpg");
+                try {
+                    if (outputImage.exists()) {
+                        Log.e(TAG, "onClick: 111");
+                        outputImage.delete();
+                    }
+                    Log.e(TAG, "onClick: 222");
+                    outputImage.createNewFile();
+                    Log.e(TAG, "onClick: 333");
+                } catch (IOException e) {
+                    Log.e(TAG, "onClick: what");
+                    e.printStackTrace();
+                }
+
+                if (Build.VERSION.SDK_INT >= 24) {
+                    Log.e(TAG, "onClick: 444");
+                    imageUri = FileProvider.getUriForFile(MainActivity.this, "com.example.cameraalbumtest.fileprovider", outputImage);
+                } else {
+                    Log.e(TAG, "onClick: 555");
+                    imageUri = Uri.fromFile(outputImage);
+                }
+
+                Log.e(TAG, "onClick: 666");
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                startActivityForResult(intent, TAKE_PHOTO);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        picture.setImageBitmap(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+}
