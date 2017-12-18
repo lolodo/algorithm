14793fb5cc77b1e3bf15912f90c034d46bfb58d5
alex <liufangyuan111@126.com>
2017-12-18 14:57:20 +0800

    gstreamer

--- android_app/OkHttpNetworkTest/app/src/main/java/com/example/alex/networktest/MainActivity.java
+++ android_app/OkHttpNetworkTest/app/src/main/java/com/example/alex/networktest/MainActivity.java
@@ -15,6 +15,12 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     TextView responseText;
@@ -31,46 +37,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+            sendRequestWithOkHttp();
         }
     }
 
-    private void sendRequestWithHttpURLConnection() {
+    private void sendRequestWithOkHttp() {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                HttpURLConnection connection = null;
-                BufferedReader reader = null;
+//                HttpURLConnection connection = null;
+
+//                BufferedReader reader = null;
                 try {
-                    URL url = new URL("https://www.baidu.com");
-                    connection = (HttpURLConnection)url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
+                    OkHttpClient client = new OkHttpClient();
+//                    URL url = new URL("https://www.baidu.com");
+//                    connection = (HttpURLConnection)url.openConnection();
+//                    connection.setRequestMethod("GET");
+//                    connection.setConnectTimeout(8000);
+//                    connection.setReadTimeout(8000);
+                    Request request = new Request.Builder().url("http://www.baidu.com").build();
+                    Response response = client.newCall(request).execute();
+                    String responseData = response.body().string();
+//                    RequestBody requestBody = new FormBody().Builder()
+//                            .add("username", "admin")
+//                            .add("passwd", "123456")
+//                            .build();
 
-                    reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder response = new StringBuilder();
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        response.append(line);
-                    }
+//                    InputStream in = connection.getInputStream();
 
-                    showResponse(response.toString());
+//                    reader = new BufferedReader(new InputStreamReader(in));
+//                    StringBuilder response = new StringBuilder();
+//                    String line;
+//                    while ((line = reader.readLine()) != null) {
+//                        response.append(line);
+//                    }
+
+                    showResponse(responseData);
                 } catch (Exception e){
                     e.printStackTrace();
-                } finally {
-                    if (reader != null) {
-                        try {
-                            reader.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                    if (connection != null) {
-                        connection.disconnect();
-                    }
                 }
             }
         }).start();
