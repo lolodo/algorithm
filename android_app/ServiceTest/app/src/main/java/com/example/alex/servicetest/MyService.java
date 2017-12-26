711a4eceaab412e9bebbbe21856a7ef418294b11
alex <liufangyuan111@126.com>
2017-12-27 01:05:36 +0800

    good

--- android_app/ServiceTest/app/src/main/java/com/example/alex/servicetest/MyService.java
+++ android_app/ServiceTest/app/src/main/java/com/example/alex/servicetest/MyService.java
@@ -2,30 +2,51 @@ package com.example.alex.servicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
+
+import static android.content.ContentValues.TAG;
 
 public class MyService extends Service {
+    private DownloadBinder mBinder = new DownloadBinder();
+
+    class DownloadBinder extends Binder {
+        public void startDownload() {
+            Log.d(TAG, "startDownload: executed");
+        }
+
+        public int getProgress() {
+            Log.d(TAG, "getProgress: executed");
+            return 0;
+        }
+
+    }
+
     public MyService() {
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG, "onCreate: executed");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(TAG, "onStartCommand: executed");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy: executed");
     }
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return mBinder;
     }
 }
