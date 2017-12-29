be4dbd1261e57febf25c5761f7ad2b2b6c87a3da
alex <liufangyuan111@126.com>
2017-12-29 21:42:45 +0800

    good

--- android_app/ServiceTest/app/src/main/java/com/example/alex/servicetest/MainActivity.java
+++ android_app/ServiceTest/app/src/main/java/com/example/alex/servicetest/MainActivity.java
@@ -6,11 +6,13 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "mainintent";
     private MyService.DownloadBinder downloadBinder;
     private ServiceConnection connection = new ServiceConnection() {
         @Override
@@ -39,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button unbindService = (Button)findViewById(R.id.unbind_service);
         bindService.setOnClickListener(this);
         unbindService.setOnClickListener(this);
+        
+        Button startIntentService = (Button)findViewById(R.id.start_intent_service);
+        startIntentService.setOnClickListener(this);
     }
 
     @Override
@@ -58,6 +63,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.unbind_service:
                 unbindService(connection);
+                break;
+            case R.id.start_intent_service:
+                Log.d(TAG, "onClick: Thread id is " + Thread.currentThread().getId());
+                Intent intentService = new Intent(this, MyIntentService.class);
+                startService(intentService);
                 break;
             default:
                 break;
