e72103c2fa7c13501c448e535355077d23bc3759
Fangyuan Liu <fangyuan.liu@nio.com>
2018-01-08 20:35:21 +0800

    newest
    Signed-off-by: Fangyuan Liu <fangyuan.liu@nio.com>

---
+++ android_app/MaterialTest/app/src/main/java/com/example/liufangyuan/materialtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.liufangyuan.materialtest;
+
+/**
+ * Created by fangyuan on 1/8/18.
+ */
+
+public class Fruit {
+	private String name;
+	private int imageId;
+
+	public Fruit(String name, int imageId) {
+		this.name = name;
+		this.imageId = imageId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getImageId() {
+		return imageId;
+	}
+}
