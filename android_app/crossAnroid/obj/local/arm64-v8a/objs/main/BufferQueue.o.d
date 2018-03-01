93a5240dfa0292f0860f4ff2d21582f4d290d766
Fangyuan Liu <fangyuan.liu@nio.com>
2018-03-01 15:22:13 +0800

    good
    Signed-off-by: Fangyuan Liu <fangyuan.liu@nio.com>

--- android_app/crossAnroid/obj/local/arm64-v8a/objs/main/BufferQueue.o.d
+++
@@ -1,7 +0,0 @@
-./obj/local/arm64-v8a/objs/main/BufferQueue.o: jni/BufferQueue.cpp \
-  /home/fangyuan/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream \
-  jni/BufferQueue.h
-
-/home/fangyuan/Android/Sdk/ndk-bundle/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
-
-jni/BufferQueue.h:
